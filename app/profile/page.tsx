7b323ebc3eba75467a815c5c6598aed9e6240bdf
Abdulganiyu Uthman <uthmanabdulganiyu2019@gmail.com>
2024-09-29 01:36:12 +0100

    [update] profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,16 @@
 import React, { useEffect } from "react";
-import { ActivityIndicator, SafeAreaView, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Dimensions,
+  SafeAreaView,
+  Text,
+  View,
+} from "react-native";
 import { useProfileState } from "./context";
 
 const ProfileScreen = () => {
   const { profile, getProfile, loading } = useProfileState();
+  const screenHeight = Dimensions.get("window").height;
 
   useEffect(() => {
     getProfile();
@@ -21,11 +28,12 @@ const ProfileScreen = () => {
 
   return (
     <SafeAreaView className="bg-secondary">
-      <View className="flex justify-center items-center text-center w-28 h-28 m-auto my-12 rounded-full bg-primary">
+      <View className="flex justify-center items-center text-center w-28 h-28 m-auto mt-12 rounded-full bg-primary">
         <Text className="font-bold text-2xl uppercase text-white">{`${profile?.firstName?.charAt(
           0
         )}${profile?.lastName?.charAt(0)}`}</Text>
       </View>
+      <Text className="font-bold text-xl capitalize my-4 text-dark text-center">{`${profile?.firstName} ${profile?.lastName}`}</Text>
     </SafeAreaView>
   );
 };
