b3c14ccf458832df3b3e4d02b9486cc7264be822
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2025-03-26 15:46:54 +0100

    [refactor] enhance profile screen UI with gradient background and improved layout

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,153 +6,99 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Image,
 } from "react-native";
 import { useProfileState } from "./context";
 import { Divider } from "@rneui/base";
 import { useAuthState } from "../auth/context";
 import { CustomText } from "../../components";
+import { LinearGradient } from "expo-linear-gradient";
 
 const ProfileScreen = () => {
   const { profile, getProfile, loading } = useProfileState();
   const { logout } = useAuthState();
 
+  // const avatarUrl = profile?.email
+  //   ? `https://www.gravatar.com/avatar/${require("crypto")
+  //       .createHash("md5")
+  //       .update(profile.email.toLowerCase().trim())
+  //       .digest("hex")}?s=200&d=identicon`
+  //   : null;
   useEffect(() => {
     getProfile();
   }, []);
 
-  // if (loading) {
-  //   return (
-  //     <ActivityIndicator
-  //       size="large"
-  //       color="#9C27B0"
-  //       style={{ flex: 1, justifyContent: "center" }}
-  //     />
-  //   );
-  // }
-
   return (
-    // <SafeAreaView className="flex-1 bg-secondary">
-    //   <ScrollView className="px-6 py-4">
-    //     {/* Avatar and Name */}
-    //     <View className="items-center mt-12">
-    //       <View className="w-28 h-28 rounded-full bg-primary flex justify-center items-center shadow-sm">
-    //         <CustomText className="font-bold text-3xl uppercase text-white">
-    //           {`${profile?.firstName?.charAt(0) || ""}${
-    //             profile?.lastName?.charAt(0) || ""
-    //           }`}
-    //         </CustomText>
-    //       </View>
-    //       <CustomText className="font-bold text-xl capitalize text-dark mt-4">
-    //         {`${profile?.firstName || ""} ${profile?.lastName || ""}`}
-    //       </CustomText>
-    //     </View>
-
-    //     {/* Profile Details */}
-    //     <View className="mt-8 bg-white rounded-xl shadow-sm border border-gray-200 p-4">
-    //       <View className="space-y-4">
-    //         <View>
-    //           <CustomText className="text-dark font-semibold text-sm">
-    //             First Name
-    //           </CustomText>
-    //           <CustomText className="text-dark text-lg capitalize mt-1">
-    //             {profile?.firstName || "N/A"}
-    //           </CustomText>
-    //         </View>
-    //         <View className="border-b border-gray-200" />
-
-    //         <View>
-    //           <CustomText className="text-dark font-semibold text-sm">Last Name</CustomText>
-    //           <CustomText className="text-dark text-lg capitalize mt-1">
-    //             {profile?.lastName || "N/A"}
-    //           </CustomText>
-    //         </View>
-    //         <View className="border-b border-gray-200" />
-
-    //         <View>
-    //           <CustomText className="text-dark font-semibold text-sm">Email</CustomText>
-    //           <CustomText className="text-dark text-lg mt-1">
-    //             {profile?.email || "N/A"}
-    //           </CustomText>
-    //         </View>
-    //         <View className="border-b border-gray-200" />
-
-    //         <View>
-    //           <CustomText className="text-dark font-semibold text-sm">Role</CustomText>
-    //           <CustomText className="text-dark text-lg capitalize mt-1">
-    //             {profile?.role || "N/A"}
-    //           </CustomText>
-    //         </View>
-    //       </View>
-
-    //       {/* Sign Out Button */}
-    //       <TouchableOpacity
-    //         onPress={logout}
-    //         className="mt-6 bg-red-500 py-3 rounded-xl items-center"
-    //       >
-    //         <CustomText className="text-white font-semibold">Sign Out</CustomText>
-    //       </TouchableOpacity>
-    //     </View>
-    //   </ScrollView>
-    // </SafeAreaView>
     <SafeAreaView className="flex-1 bg-secondary">
-      <ScrollView className="px-6 py-4">
-        <View className="bg-white rounded-xl shadow-sm border border-gray-200 p-4 items-center mt-8">
-          <View className="w-32 h-32 rounded-full bg-primary flex justify-center items-center shadow-md">
-            <CustomText className="font-bold text-4xl uppercase text-white font-space-mono">
-              {`${profile?.firstName?.charAt(0) || ""}${
-                profile?.lastName?.charAt(0) || ""
-              }`}
-            </CustomText>
+      <ScrollView>
+        <LinearGradient
+          colors={["#4caf50", "#6b7280"]}
+          className="h-52 flex justify-center items-center"
+        >
+          <View className=" self-center">
+            <View className="w-28 h-28 rounded-full bg-primary border-4 border-white shadow-md flex justify-center items-center overflow-hidden">
+              <Text className="font-sans text-4xl text-white uppercase">
+                {`${profile?.firstName?.charAt(0) || ""}${
+                  profile?.lastName?.charAt(0) || ""
+                }`}
+              </Text>
+            </View>
           </View>
-          <CustomText
-            className="font-bold text-2xl capitalize text-dark mt-4 font-space-mono"
-          >
+          {/* <CustomText className="text-2xl text-white capitalize">
             {`${profile?.firstName || ""} ${profile?.lastName || ""}`}
           </CustomText>
-          <CustomText className="text-gray-500 text-sm capitalize mt-1 font-space-mono">
+          <CustomText className="text-sm text-white capitalize mt-1">
             {profile?.role || "N/A"}
-          </CustomText>
-        </View>
+          </CustomText> */}
+        </LinearGradient>
 
-        <View className="bg-white rounded-xl shadow-sm border border-gray-200 p-4 mt-6">
-          <View className="space-y-3">
-            <View className="flex-row justify-between">
-              <CustomText className="text-gray-500 text-sm font-semibold font-space-mono">
+        {/* Details Card */}
+        <View className="bg-white rounded-t-3xl shadow-sm border border-gray-200 mx-6 pt-16 pb-6">
+          <View className="px-4 space-y-4">
+            <View className="flex-row justify-between items-center">
+              <CustomText className="text-sm text-gray-500">
                 First Name
               </CustomText>
-              <CustomText className="text-dark text-base capitalize font-space-mono">
+              <CustomText className="text-base text-dark capitalize">
                 {profile?.firstName || "N/A"}
               </CustomText>
             </View>
-            <View className="flex-row justify-between">
-              <CustomText className="text-gray-500 text-sm font-semibold font-space-mono">
+            <View className="border-b border-gray-200" />
+
+            <View className="flex-row justify-between items-center">
+              <CustomText className="text-sm text-gray-500">
                 Last Name
               </CustomText>
-              <CustomText className="text-dark text-base capitalize">
+              <CustomText className="text-base text-dark capitalize">
                 {profile?.lastName || "N/A"}
               </CustomText>
             </View>
-            <View className="flex-row justify-between">
-              <CustomText className="text-gray-500 text-sm font-semibold">Email</CustomText>
-              <CustomText className="text-dark text-base">
+            <View className="border-b border-gray-200" />
+
+            <View className="flex-row justify-between items-center">
+              <CustomText className="text-sm text-gray-500">Email</CustomText>
+              <CustomText className="text-base text-dark">
                 {profile?.email || "N/A"}
               </CustomText>
             </View>
-            <View className="flex-row justify-between">
-              <CustomText className="text-gray-500 text-sm font-semibold">Role</CustomText>
-              <CustomText className="text-dark text-base capitalize">
+            <View className="border-b border-gray-200" />
+
+            <View className="flex-row justify-between items-center">
+              <CustomText className="text-sm text-gray-500">Role</CustomText>
+              <CustomText className="text-base text-dark capitalize">
                 {profile?.role || "N/A"}
               </CustomText>
             </View>
           </View>
-        </View>
 
-        <TouchableOpacity
-          onPress={logout}
-          className="mt-8 border border-red-500 py-3 rounded-xl items-center"
-        >
-          <CustomText className="text-red-500 font-semibold text-base">Sign Out</CustomText>
-        </TouchableOpacity>
+          {/* Sign Out Button */}
+          <TouchableOpacity
+            onPress={logout}
+            className="mt-6 mx-4 border border-red-500 py-3 rounded-xl items-center shadow-sm"
+          >
+            <CustomText className="text-base text-red-500">Sign Out</CustomText>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
